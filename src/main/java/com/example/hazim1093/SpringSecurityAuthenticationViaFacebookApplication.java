da589780e14db249c7fd5341699ce17727f1674d
Hazim <hazim@aurorasolutions.io>
2016-04-26 06:15:08 +0500

    Initial SpringBoot Project structure

---
+++ src/main/java/com/example/hazim1093/SpringSecurityAuthenticationViaFacebookApplication.java
@@ -0,0 +1,12 @@
+package com.example.hazim1093;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAuthenticationViaFacebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityAuthenticationViaFacebookApplication.class, args);
+	}
+}
